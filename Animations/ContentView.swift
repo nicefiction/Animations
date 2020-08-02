8241a0d2b6aeeed18f2ec875428ed99421c21675
Olivier Van hamme <nicefiction@gmail.com>
2020-08-02 19:44:37 +0200

    Initial Commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Olivier Van hamme on 02/08/2020.
+//  Copyright © 2020 nicefiction. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
