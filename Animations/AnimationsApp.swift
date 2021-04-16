faad4d9979f9133b50e26880fd47113375b48c77
Olivier Van hamme <nicefiction@gmail.com>
2021-04-16 08:58:27 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Olivier Van hamme on 16/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
