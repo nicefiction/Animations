0ca3dc402880d88b68159d2e2833063b15de9c68
Olivier Van hamme <nicefiction@gmail.com>
2021-04-16 10:44:06 +0200

    Day 32 , customizes animations .

---
+++ Animations/Day32/CustomizingAnimations_Part1.swift
@@ -0,0 +1,143 @@
+//  CustomizingAnimations.swift
+/**
+ SOURCE :
+ https://www.hackingwithswift.com/books/ios-swiftui/customizing-animations-in-swiftui
+ 
+ When we attach the modifier `.animation(.default)` to a view ,
+ SwiftUI will automatically animate any changes that happen to that view
+ using whatever is the default system animation .
+ In practice , that is an “ease in, ease out” animation ,
+ which means iOS will start the animation slow ,
+ make it pick up speed ,
+ then slow down as it approaches its end .
+ We can control the type of animation used
+ by passing in different values to the modifier .
+ */
+
+import SwiftUI
+
+
+
+struct CustomizingAnimations: View {
+    
+     // ////////////////////////
+    //  MARK: PROPERTY WRAPPERS
+    
+    @State private var animationAmount: CGFloat = 1.00
+    
+    
+    
+     // //////////////////////////
+    //  MARK: COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        VStack {
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            // .blur(radius : (animationAmount - 1) * 2)
+            .animation(.default)
+            
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            // .blur(radius : (animationAmount - 1) * 2)
+            /**
+             Use `.easeOut` to make the animation start fast
+             then slow down to a smooth stop :
+             */
+            .animation(.easeOut)
+            
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.yellow)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            // .blur(radius : (animationAmount - 1) * 2)
+            /**
+             Spring animations cause the movement to overshoot
+             then return to settle at its target .
+             You can control the initial `stiffness` of the spring
+             ( which sets its initial velocity when the animation starts ) ,
+             and also how fast the animation should be _damped_
+             – lower values cause the spring to bounce back and forth for longer .
+             */
+            .animation(.interpolatingSpring(stiffness : 50.00 ,
+                                            damping : 1.00))
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.orange)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            // .blur(radius : (animationAmount - 1) * 2)
+            /**
+             For more precise control ,
+             we can customize the animation with a duration
+             specified as a number of seconds :
+             */
+            .animation(.easeInOut(duration : 2.00))
+            
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.green)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            // .blur(radius : (animationAmount - 1) * 2)
+            /**
+             When we say `.easeInOut(duration : 2)`
+             we are actually creating an instance of an `Animation` struct
+             that has its own set of modifiers . So ,
+             we can attach modifiers directly to the animation
+             to add a delay like this :
+             */
+            .animation(
+                Animation
+                    .easeInOut(duration : 2.00)
+                    .delay(1.00)
+            )
+            /**
+             `NOTICE` that we explicitly have to say `Animation.easeInOut()` now ,
+             because otherwise Swift isn’t quite sure what we mean :
+             */
+        }
+        .font(.system(size : 18 ,
+                      weight : .semibold ,
+                      design : .rounded))
+    }
+}
+
+
+
+
+
+struct CustomizingAnimations_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        CustomizingAnimations().previewDevice("iPhone 12 Pro")
+    }
+}
---
+++ Animations/Day32/CustomizingAnimations_Part2.swift
@@ -0,0 +1,120 @@
+//  CustomizingAnimations_Part2.swift
+
+import SwiftUI
+
+
+
+struct CustomizingAnimations_Part2: View {
+    
+     // ////////////////////////
+    //  MARK: PROPERTY WRAPPERS
+    
+    @State private var animationAmount: CGFloat = 1.00
+    
+    
+    
+     // //////////////////////////
+    //  MARK: COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        VStack {
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            .animation(
+                Animation
+                    .easeInOut(duration : 2.00)
+                    .repeatCount(3 ,
+                                 autoreverses : true)
+            )
+            /**
+             `NOTE`: If we had set repeat count to `2`
+             then the button would scale up
+             then down again ,
+             then jump immediately back up to its larger scale .
+             This is because
+             ultimately
+             the button must match the state of our program ,
+             regardless of what animations we apply
+             – when the animation finishes
+             the button must have whatever value is set in `animationAmount` .
+             */
+            
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            /**
+             For continuous animations ,
+             there is a `repeatForever()` modifier
+             that can be used like this :
+             */
+            .animation(
+                Animation
+                    .easeInOut(duration : 2.00)
+                    .repeatForever()
+            )
+            /**
+             `NOTE`: We can use these `repeatForever()` animations
+             in combination with `onAppear()`
+             to make animations that start immediately
+             and continue animating
+             for the life of the view .
+             
+             To demonstrate this ,
+             we are going to remove the animation from the button itself
+             and instead apply it an overlay
+             to make a sort of pulsating circle around the button :
+             */
+            
+            Button("Tap Me") {
+                // animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.yellow)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            // .scaleEffect(animationAmount)
+            .overlay(
+                Circle()
+                    .strokeBorder(Color.orange)
+                    .scaleEffect(animationAmount)
+                    // .opacity(Double(2 - animationAmount))
+                    .animation(
+                        Animation
+                            .easeInOut(duration : 1.00)
+                            .repeatForever(autoreverses : false)
+                    )
+            )
+            .onAppear {
+                self.animationAmount = 2
+            }
+        }
+        .font(.system(size : 18.00 ,
+                      weight : .semibold ,
+                      design : .rounded))
+    }
+}
+
+
+
+
+
+struct CustomizingAnimations_Part2_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        CustomizingAnimations_Part2().previewDevice("iPhone 12 Pro")
+    }
+}
